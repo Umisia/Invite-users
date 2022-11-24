f02716f1c20c5470eaf5f33288d67f26c5b32e25
umisia <maciaszek89@gmail.com>
2022-11-24 14:04:52 +0000

    minor changes

--- Comma Separation.py
+++ Comma Separation.py
@@ -5,18 +5,14 @@ try:
 except:
     option = None
     
-lines = pyperclip.paste().replace('(', ' (').split()
+lines = pyperclip.paste().replace('(', ' (').replace('"', ' ').split()
 
 new_list = [li for li in lines if "@" in li]
 new_list = [li for li in new_list if "@name.com" not in li]
 new_list2 = []
 for el in new_list:
-    el = el.replace("<","").replace(">","")
-    if ")" in el:
-        el= el[el.index(")")+1:]
-        new_list2.append(el)
-    else:
-        new_list2.append(el)
+    el = el.replace("<","").replace(">","").replace(")", "").replace("(", "").replace(";", "")
+    new_list2.append(el)
 
 if option == "c" or option is None: #default
     pyperclip.copy(", ".join(new_list2).replace(',,', ','))
